e62b5d9bbe7c5f632c28c47eeab348aa0dd80e43
NutellaDev <eduardocayovilca@gmail.com>
2026-06-02 23:40:32 -0500

    feat: strengthen B2B SEO and lead conversion flow

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ function revealFallback() {
 
 let menuInitialized = false;
 const analyticsConsentKey = 'redline_analytics_consent';
-const gaMeasurementId = 'G-EB9CT1636Z';
+const gtmId = 'GTM-5SXFJLSV';
 
 function clearBootTimeout() {
   window.clearTimeout(window.__redlineBootTimer);
@@ -24,9 +24,9 @@ function clearBootTimeout() {
 }
 
 function trackEvent(eventName, params = {}) {
-  if (typeof window.gtag !== 'function') return;
-
-  window.gtag('event', eventName, {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: eventName,
     page_path: window.location.pathname,
     ...params
   });
@@ -34,20 +34,16 @@ function trackEvent(eventName, params = {}) {
 
 function loadAnalytics() {
   if (window.__redlineAnalyticsLoaded) return;
-
   window.__redlineAnalyticsLoaded = true;
+
   window.dataLayer = window.dataLayer || [];
-  window.gtag = function gtag() {
-    window.dataLayer.push(arguments);
-  };
 
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`;
-  document.head.appendChild(script);
-
-  window.gtag('js', new Date());
-  window.gtag('config', gaMeasurementId);
+  // Google Tag Manager
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer',gtmId);
 }
 
 function getStoredAnalyticsConsent() {
@@ -216,7 +212,10 @@ async function handleLeadSubmit(event, form) {
 
     trackEvent('generate_lead', { form_name: formName });
     form.reset();
-    setFormMessage(form, 'Solicitud enviada. Te responderemos en menos de 24 horas hábiles.', 'success');
+    setFormMessage(form, 'Solicitud enviada. Redirigiendo...', 'success');
+    window.setTimeout(() => {
+      window.location.href = 'gracias.html';
+    }, 400);
   } catch (error) {
     console.warn('Web3Forms AJAX failed, falling back to native submit.', error);
     trackEvent('form_submit_fallback', { form_name: formName });
